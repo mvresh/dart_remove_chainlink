a44f20e4237e175ce3183d21c1c95048defe8c6b
Varun Resh Maddali <mvresh@gmail.com>
2019-10-03 16:14:50 +0530

    completed

--- bin/main.dart
+++ bin/main.dart
@@ -24,10 +24,10 @@ main() {
   Link sixthLink = Link('Parvati');
   fifthLink.next = sixthLink;
 
-  print(firstLink);
+  printChain(firstLink);
   print('-------');
   removeLink(firstLink, 'Makiko');
-  print(firstLink);
+  printChain(firstLink);
 }
 
 // Printing the linkTree
@@ -41,6 +41,18 @@ void printChain(Link link) {
 
 // Exercise: Write a function which takes in a link and a word. It should remove the link with value matching word
 void removeLink(Link firstLink, String word) {
+  Link currLink = firstLink;
+
+  while(currLink.next != null){
+
+    if(currLink.next.value == word)
+    {
+      currLink.next = currLink.next.next;
+    }
+    currLink = currLink.next;
+  }
+
+
   // Implement function here
 
 }
